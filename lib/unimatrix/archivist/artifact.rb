3c86c48c913750a561c3433ddff9931bced8a1d1
Nick 'Greenie' Greenfield <greenie0506@gmail.com>
2017-12-13 11:42:53 -0800

    Add more fields to be serialized on an artifact

--- lib/unimatrix/archivist/artifact.rb
+++ lib/unimatrix/archivist/artifact.rb
@@ -1,9 +1,18 @@
 module Unimatrix::Archivist
 
   class Artifact < Unimatrix::DynamicResource
-
+    field    :creator_uuid
+    field    :picture_uuid
+    field    :provider
+    field    :provider_uid
+    field    :provider_url
+    field    :description
+    field    :short_description
+    field    :slug
+    field    :note
     field    :type_name
     field    :component_ids
+    field    :relationships
     field    :name
     field    :created_at
     field    :updated_at
