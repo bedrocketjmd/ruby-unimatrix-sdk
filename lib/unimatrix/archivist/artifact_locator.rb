a3848d0c0c2d73b34c9647bfedcb7c76c49dddb4
Nick 'Greenie' Greenfield <greenie0506@gmail.com>
2017-12-13 16:13:07 -0800

    Adding artifact locator

---
+++ lib/unimatrix/archivist/artifact_locator.rb
@@ -0,0 +1,22 @@
+module Unimatrix::Archivist
+
+  class ArtifactLocator < Unimatrix::DynamicResource
+    
+    field      :id
+    field      :uuid
+    field      :provider
+    field      :type_name
+    field      :realm_uuid
+    field      :created_at
+    field      :updated_at
+    field      :artifact_uuid
+    field      :provider_url
+    field      :provider_uid
+    field      :destroyed_at
+    
+    has_one  :artifact
+    has_many :errors
+
+  end
+
+end
