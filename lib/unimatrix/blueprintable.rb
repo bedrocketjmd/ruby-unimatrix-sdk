61939106a3c4b579c9ce71b4856793342b18803f
jacksonsouza <jacksonsouza@me.com>
2018-01-18 13:44:27 -0800

    post unscoped blueprint

--- lib/unimatrix/blueprintable.rb
+++ lib/unimatrix/blueprintable.rb
@@ -8,8 +8,32 @@ module Unimatrix
 
     def initialize( attributes = {}, associations = {} )
 
+      blueprint = unimatrix_operation(
+        "/realms/#{ attributes[ "realm_uuid" ] }/blueprints"
+      ).where( {
+        "resource_type_name": attributes[ "type_name" ]
+      } ).include (
+        "blueprint_attributes"
+      ).read.first
+
       binding.pry
 
+    end
+
+    protected; def token
+      @authorization_token ||= begin
+         Unimatrix::Authorization::ClientCredentialsGrant.new(
+           client_id:     ENV[ 'KEYMAKER_CLIENT' ],
+           client_secret: ENV[ 'KEYMAKER_SECRET' ]
+         ).request_token
+      end
+    end
+
+    protected; def unimatrix_operation( endpoint, args = {} )
+      Unimatrix::Operation.new(
+          endpoint,
+          args.merge( { access_token: token } )
+      )
     end
 
   end
