0f93547bea3c2b99167990c902bd085eb9f21d8d
Kristoph <git@kristoph.net>
2018-02-28 03:10:08 -0500

    modified version management so there is only one actual version number; bumped to 2.2.2.beta

--- lib/unimatrix/version.rb
+++ lib/unimatrix/version.rb
@@ -1,3 +1,9 @@
 module Unimatrix
-  VERSION = "2.5.0"
+  VERSION = begin
+    version = File.read( File.join( __dir__, "../../VERSION" ) )
+    version.scan( /\A.*/ ).first
+  rescue 
+    raise "Error: The Unimatrix SDK VERSION file is missing or invalid."
+    exit( 1 )
+  end
 end
