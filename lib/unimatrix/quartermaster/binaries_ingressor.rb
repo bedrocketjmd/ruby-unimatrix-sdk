2491ced6aee7bc144101d782a80a85fa2dfc7e2b
Juan Pol <juan@sportsrocket.com>
2018-03-01 16:49:11 -0500

    SR-1254 removing binary_id and activity_id from binaries_ingressor model

--- lib/unimatrix/quartermaster/binaries_ingressor.rb
+++ lib/unimatrix/quartermaster/binaries_ingressor.rb
@@ -4,13 +4,11 @@ module Unimatrix::Quartermaster
 
     field      :id
     field      :binary_uuid
-    field      :activity_id
     field      :state
     field      :properties
     field      :created_at
     field      :updated_at
     field      :uuid
-    field      :binary_id
     field      :url
 
   end
