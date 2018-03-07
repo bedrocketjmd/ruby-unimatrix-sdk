a03510e7648760d394c73f4fe535afcf745054aa
zerogee00 <david@bedrocket.com>
2018-03-07 13:27:34 -0500

    add read_only

--- lib/unimatrix/quartermaster/binary.rb
+++ lib/unimatrix/quartermaster/binary.rb
@@ -2,17 +2,16 @@ module Unimatrix::Quartermaster
 
   class Binary < Unimatrix::DynamicResource
 
-    field :id
     field :filename
     field :content_length
     field :content_type
-    field :realm_uuid
+    field :realm_uuid,  read_only: true
     field :created_at,  read_only: true
     field :updated_at,  read_only: true
     field :uuid
     field :storage_key
 
-    has_many :binaries_ingressors
+    has_one :binaries_ingressor
 
   end
 
