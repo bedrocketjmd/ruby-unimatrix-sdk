c6ee95976a9cfd21a21efa874078a707f7445510
Matthew Yang <yang70@gmail.com>
2018-03-20 15:51:33 -0700

    go back to hard coded version file

--- lib/unimatrix/version.rb
+++ lib/unimatrix/version.rb
@@ -1,9 +1,3 @@
 module Unimatrix
-  VERSION = begin
-    version = File.read( File.join( __dir__, "../../VERSION" ) )
-    version.scan( /\A.*/ ).first
-  rescue 
-    raise "Error: The Unimatrix SDK VERSION file is missing or invalid."
-    exit( 1 )
-  end
+  VERSION = "2.5.2"
 end
