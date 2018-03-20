6f670184a88fabc31cb13f7df0b807f970ca5eaa
Nick 'Greenie' Greenfield <greenie0506@gmail.com>
2018-03-20 16:46:46 -0700

    Fix location version

--- lib/unimatrix/version.rb
+++ lib/unimatrix/version.rb
@@ -1,7 +1,6 @@
 module Unimatrix
   VERSION = begin
     version = File.read( File.join( __dir__, "../../VERSION" ) )
-    version.scan( /\A.*/ ).first
   rescue 
     raise "Error: The Unimatrix SDK VERSION file is missing or invalid."
     exit( 1 )
