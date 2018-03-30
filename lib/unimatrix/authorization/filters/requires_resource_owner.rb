dcbf5773696f40df79094171ffcd3e6d16f6a34a
Alena Wang <alena.n.wang@gmail.com>
2018-03-30 17:01:58 -0400

    Remove resource param from requires resource owner filter operation, remove resource argument from method

--- lib/unimatrix/authorization/filters/requires_resource_owner.rb
+++ lib/unimatrix/authorization/filters/requires_resource_owner.rb
@@ -1,33 +1,13 @@
 module Unimatrix::Authorization
-  
+
   class RequiresResourceOwner
-    def initialize( resource, options = {} )
-      @resource_name = resource
-      @resource_server = options[ :resource_server ] || ENV[ 'APPLICATION_NAME' ]
-    end
-    
+
     def before( controller )
       access_token = controller.params[ 'access_token' ]
-      
-      realm_uuid = begin 
-        if controller.respond_to?( :realm_uuid )
-          controller.realm_uuid
-        elsif controller.respond_to?( :realm )
-          controller.realm.uuid
-        else
-          controller.params[ :realm_uuid ]
-        end
-      end
 
       if access_token.present?
-        resource_owner =
-          controller.request_resource_owner( 
-            @resource_name, 
-            access_token, 
-            realm_uuid, 
-            @resource_server 
-          )
-          
+        resource_owner = controller.request_resource_owner( access_token )
+
         if resource_owner.present? && resource_owner.is_a?( Array ) &&
            resource_owner.first.type_name == 'resource_owner'
           controller.resource_owner = resource_owner
@@ -38,23 +18,23 @@ module Unimatrix::Authorization
           )
         end
       else
-        controller.render_error( 
+        controller.render_error(
           ::MissingParameterError,
           "The parameter 'access_token' is required."
-        ) 
+        )
       end
     end
   end
 
   module ClassMethods
-    def requires_resource_owner( resource, options = {} )
+    def requires_resource_owner( options = {} )
       before_action(
-        RequiresResourceOwner.new( resource, options ),
+        RequiresResourceOwner.new,
         options
       )
     end
   end
-  
+
   def self.included( controller )
     controller.extend( ClassMethods )
   end
@@ -65,23 +45,16 @@ module Unimatrix::Authorization
 
   def resource_owner
     @resource_owner ||= begin
-      request_resource_owner( 
-        @resource_name,
-        params[ :access_token ],
-        realm_uuid,
-        @resource_server
-      )
+      request_resource_owner( params[ :access_token ] )
     end
   end
-  
-  def request_resource_owner( resource_name, access_token, realm_uuid, resource_server )
-    if resource_name && access_token
-      realm_uuid = realm_uuid || '*'
+
+  def request_resource_owner( access_token )
+    if access_token
       Operation.new( '/resource_owner' ).where(
-        access_token: access_token,
-        resource: "realm/#{ realm_uuid }::#{ resource_server }::#{ resource_name }/*"
+        access_token: access_token
       ).read
     end
   end
-    
+
 end
