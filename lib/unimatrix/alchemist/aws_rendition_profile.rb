a509f465031b923f8fd667f20540ff6443ba95a1
Asher Kory <asher.kory@gmail.com>
2018-05-11 12:34:07 -0400

    Add MediaConvert support fields to Alchemist AwsRenditionProfile

--- lib/unimatrix/alchemist/aws_rendition_profile.rb
+++ lib/unimatrix/alchemist/aws_rendition_profile.rb
@@ -3,6 +3,12 @@ module Unimatrix::Alchemist
   class AwsRenditionProfile < RenditionProfile
 
     field :preset_id
+    field :transcoder 
+    field :encryption
+    field :encryption_system_id
+    field :encryption_url
+    field :encryption_static_key
+    field :encryption_static_url
 
   end
 
