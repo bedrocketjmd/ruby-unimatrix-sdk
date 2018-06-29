ed7489e6f56b6b39f315bf8dca65e9b314914fe5
Jaclyn Casper <casper.jaclyn@gmail.com>
2018-06-29 12:46:03 -0400

    ARC-450 Update archivist fields

--- lib/unimatrix/archivist/component.rb
+++ lib/unimatrix/archivist/component.rb
@@ -1,14 +1,15 @@
 module Unimatrix::Archivist
 
   class Component < Unimatrix::DynamicResource
-    
+
     field    :id
     field    :uuid
     field    :realm_uuid
     field    :type_name
     field    :created_at
     field    :updated_at
-    
+    field    :destroyed_at
+
     has_one  :artifact
     has_many :errors
 
